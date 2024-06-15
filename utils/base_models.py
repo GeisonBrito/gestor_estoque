ce36fecb52402932c9c5d37f86d7e455d40819e4
Geison <geisonbrito@icloud.com>
2024-06-14 21:10:49 -0300

    atualizacao da aula dia 14_06

---
+++ utils/base_models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class BaseModel(models.Model):
+    data_criacao = models.DateField(
+        auto_now_add=True,
+    )
+    data_atualizacao = models.DateField(
+        auto_now=True,
+    )
+
+    class Meta: 
+        abstract = True,
