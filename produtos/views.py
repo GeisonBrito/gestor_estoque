917f17dc81f561435a6d2813eceac13f9ce92e96
Geison <geisonbrito@icloud.com>
2024-07-03 17:41:59 -0300

    botao editar e excluir

--- produtos/views.py
+++ produtos/views.py
@@ -1,8 +1,9 @@
 
 from django.shortcuts import redirect, render
 
-from .forms import LocalForm
-from .models import Local
+from .forms import LocalForm, EmbalagemForm
+from .models import Local, Embalagem
+
 
 def inicio(request):
     return render(request, 'menu.html')
@@ -25,4 +26,41 @@ def adicionar_local(request):
             return redirect('listar_locais')
     else:
         form = LocalForm()
-    return render(request, 'produtos/adicionar_local.html', {'form': form})        
+    return render(request, 'produtos/adicionar_local.html', {'form': form})
+
+def listar_embalagens(request):
+    consulta = request.GET.get('q')
+    sigla = request.GET.get('sigla')
+    embalagens = Embalagem.objects.all()
+    if consulta:
+        embalagens = embalagens.filter(nome__icontains=consulta)
+    if sigla:
+        embalagens = embalagens.filter(sigla=sigla)
+    return render(request, 'produtos/listar_embalagens.html', {'embalagens': embalagens})
+
+def adicionar_embalagem(request):
+    if request.method == 'POST':
+        form = EmbalagemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_embalagens')
+    else:
+        form = EmbalagemForm()
+    return render(request, 'produtos/adicionar_embalagem.html', {'form': form})
+
+def editar_embalagem(request, id):
+    embalagem = Embalagem.objects.filter(id=id).first()
+    if request.method == 'POST':
+        form = EmbalagemForm(request.POST, instance=embalagem)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_embalagens')
+    else:
+        form = EmbalagemForm(instance=embalagem)
+    return render(request, 'produtos/adicionar_embalagem.html', {'form': form}) 
+
+def excluir_embalagem(request, id):
+    embalagem = Embalagem.objects.filter(id=id).first()
+    if request.method == 'GET':
+        embalagem.delete()
+        return redirect('listar_embalagens')
